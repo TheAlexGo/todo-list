9d019c3dc0a8d1b8feb8ecdef4c16403fa266f8b
Aleksandr Gordeev <alexander.gordeev@vk.team>
2023-08-24 01:24:15 +0300

    Начало вёрстки карточек. ProgressBar в линию

---
+++ src/components/CompletedTaskCard/CompletedTaskCard.tsx
@@ -0,0 +1,23 @@
+import React, { FC, JSX } from 'react';
+
+import cn from 'classnames';
+
+import { ProgressBarLine } from '@components/ProgressBar/components/ProgressBarLine/ProgressBarLine';
+import { PropsWithClassname } from '@types';
+
+import classes from './CompletedTaskCard.module.scss';
+
+interface ICompletedTaskCard extends PropsWithClassname {
+    title: string;
+}
+
+export const CompletedTaskCard: FC<ICompletedTaskCard> = ({ title, className }): JSX.Element => {
+    const rootClasses = cn(classes['card'], className);
+
+    return (
+        <article className={rootClasses}>
+            <h3>{title}</h3>
+            <ProgressBarLine percentage={36} />
+        </article>
+    );
+};
--- src/pages/General/General.tsx
+++ src/pages/General/General.tsx
@@ -2,11 +2,21 @@ import React, { FC, JSX } from 'react';
 
 import { Greetings } from '@components/Greetings/Greetings';
 import { TaskCard } from '@components/TaskCard/TaskCard';
+import { CompletedTaskCard } from '@components/CompletedTaskCard/CompletedTaskCard';
+
+import classes from './General.module.scss';
 
 export const General: FC = (): JSX.Element => {
     return (
         <>
             <Greetings />
+            <ol className={classes['slider']}>
+                {Array.from(Array(10).keys()).map((i) => (
+                    <li className={classes['task-completed']} key={i}>
+                        <CompletedTaskCard className={classes['task-card']} title="Real Estate Website Design" />
+                    </li>
+                ))}
+            </ol>
             <TaskCard
                 id={window.crypto.randomUUID()}
                 title="Сделать проект DayTask"
